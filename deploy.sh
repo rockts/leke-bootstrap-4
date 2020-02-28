970f41ebf2c7a87bd7cf5178086685657c195d8c
rockts <rockts@sina.com>
2020-02-28 12:08:39 +0800

    修改 deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROJECT_DIR = '/var/www/html'
+echo 'start'
+cd $PROJECT_DIR
+echo 'pull code'
+git reset --hard origin/master && git clean -f
+git pull && git checkout master
+echo 'run npm script'
+npm run build
+echo 'finished'
