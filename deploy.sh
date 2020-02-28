f3f8fc1bf5247721e613d02e69944d0c87132b1b
rockts <rockts@sina.com>
2020-02-28 20:44:13 +0800

    修改 deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-PROJECT_DIR = '/var/www/html'
+#! /bin/bash
+PROJECT_DIR='/var/www/html'
+
 echo 'start'
 cd $PROJECT_DIR
+
 echo 'pull code'
-git reset --hard origin/master && git clean -f
-git pull && git checkout master
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+
 echo 'run npm install'
 npm install
+
 echo 'finished'
