fc1564fd6fc36e63913eb44fe4ad28dd80d20d97
rockts <rockts@sina.com>
2020-02-28 14:30:24 +0800

    修改 webhooks 文件

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
 var http = require("http");
-var createHandler = require("node-github-webhook");
-var handler = createHandler({ path: "/app", secret: "123456" }); // single handler
-function execFunc(content) {
-  var exec = require("child_process").exec;
-  exec(content, function(error, stdout, stderr) {
-    if (error) {
-      console.error("exec error:" + error);
-      return;
+var crypto = require("crypto");
+var exec = require("child_process").exec;
+
+// 在Webhook中设定的secret
+var secret = "123456";
+// 在Webhook中设定的Payload URL
+var url = "/app";
+
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end();
+
+    if (
+      request.headers["x-github-event"] &&
+      request.headers["x-github-event"] === "push"
+    ) {
+      console.log("push");
+
+      request.on("data", function(chunk) {
+        var Signature = request.headers["x-hub-signature"];
+        //console.log(chunk.toString()); chunk中存储了payload的数据,如果需要可以拿出来做更精确的处理.比如部署触发该次push的commit的代码
+        if (
+          verifySecret(Signature, sign(secret, chunk.toString())) &&
+          verifyUrl(url, request.url)
+        ) {
+          console.log("verify");
+          runCommand();
+        } else {
+          console.log("verify faild");
+        }
+      });
     }
-    console.log("stdout:" + stdout);
-    console.log("stderr:" + stderr);
+  })
+  .listen(7777, "127.0.0.1"); // 对,服务监听的是内网地址.用Nginx反代一下就好.(当然直接丢到外网也没问题)
+
+function sign(secret, data) {
+  return (
+    "sha1=" +
+    crypto
+      .createHmac("sha1", secret)
+      .update(data)
+      .digest("hex")
+  );
+}
+
+function verifySecret(data0, data1) {
+  return data0 == data1;
+}
+
+function verifyUrl(data0, data1) {
+  return data0 == data1;
+}
+
+function runCommand() {
+  exec("./deploy.sh", function(err, stdout, stderr) {
+    if (err) {
+      console.log("error:" + stderr);
+    } else {
+      console.log("stdout:" + stdout);
+    }
   });
 }
-http
-  .createServer(function(req, res) {
-    handler(req, res, function(err) {
-      res.statusCode = 404;
-      res.end("no such location");
-    });
-  })
-  .listen(7777);
-handler.on("error", function(err) {
-  console.error("Error:", err.message);
-});
-handler.on("push", function(event) {
-  console.log(
-    "Received a push event for %s to %s",
-    event.payload.repository.name,
-    event.payload.ref
-  );
-  execFunc("sh ./deploy.sh");
-});
